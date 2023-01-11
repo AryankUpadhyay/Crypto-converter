f55d4cfbe18706a8026333754254dc50a2c26a7d
AryankUpadhyay <iamaryankupadhyay@gmail.com>
2023-01-11 23:35:33 +0530

    added tables

--- crypto_app/src/CryptoConverter.js
+++ crypto_app/src/CryptoConverter.js
@@ -4,6 +4,63 @@ const CryptoConverter = () =>{
   return (
     <div className='crypto-converter'>
       <h2> CryptoConverter</h2>
+      <div>
+      <table>
+        <body>
+          <tr>
+            <td>Primary Converter</td>
+            <td>
+              <input
+              type="number"
+              name="currency-ammount-1"
+              value={""}
+              />
+            </td>
+
+            <td>
+              <select
+              type="number"
+              name="currency-option-1"
+              className="currency-options"
+              value={""}
+              >
+              <option></option>
+              
+              </select>
+            </td>
+          </tr>
+
+
+          <tr>
+            <td>Secondary Converter</td>
+            <td>
+              <input
+              type="number"
+              name="currency-ammount-2"
+              value={""}
+              />
+            </td>
+
+            <td>
+              <select
+              type="number"
+              name="currency-option-2"
+              className="currency-options"
+              value={""}
+              >
+              <option></option>
+              
+              </select>
+            </td>
+            
+          </tr>
+
+
+
+        </body>
+      </table>
+      </div>
+
       <ExchangeRate/>
     </div>
   )
